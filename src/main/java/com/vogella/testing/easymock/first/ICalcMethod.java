7b748e49f4c591571d8978272c0e8054e080aedf
Fanon Jupkwo <jupsfan@gmail.com>
2022-09-12 10:47:49 +0100

    EsasyMock Tutorial completed

---
+++ src/main/java/com/vogella/testing/easymock/first/ICalcMethod.java
@@ -0,0 +1,7 @@
+package com.vogella.testing.easymock.first;
+
+public interface ICalcMethod {
+    
+    double calc(Position position);
+
+}
---
+++ src/main/java/com/vogella/testing/easymock/first/Position.java
@@ -0,0 +1,7 @@
+package com.vogella.testing.easymock.first;
+
+public enum Position {
+    
+    BOSS, PROGRAMMER, SURFER
+
+}
